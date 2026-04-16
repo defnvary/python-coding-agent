8ead9a8661ca169d6300dcbfe3d0acd48b50b29a
defnvary <defnvary@gmail.com>
2026-04-16 19:33:08 +0530

    setup a basic chatbot

---
+++ main.py
@@ -0,0 +1,43 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("pass a prompt as argument!")
+        sys.exit(2)
+    prompt = sys.argv[1]
+
+    verbose_flag = False
+    if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
+        verbose_flag = True
+  
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)]),
+    ]
+    
+    response = client.models.generate_content(
+        model="gemini-3-flash-preview",
+        contents=messages,
+    )
+    
+    # print(f"API Key: {api_key}")
+    print(response.text)
+    if response is None or response.usage_metadata is None:
+        print("response is malformed")
+        return
+
+    if verbose_flag:
+        if messages[-1].parts is not None:
+            print(f"User prompt: {messages[-1].parts[0].text}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+main()
